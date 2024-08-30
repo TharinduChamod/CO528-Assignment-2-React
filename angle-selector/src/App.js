de5625cd9ad2a52322bd11bf602d06a52361d2a3
Tharindu Chamod <e18285@eng.pdn.ac.lk>
2024-08-30 14:42:42 +0530

    Initial commit
    Added initial files and configurations

---
+++ angle-selector/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./index.css";
+import AngleSelector from "./components/AngleSelector";
+
+function App() {
+  return (
+    <div className="App">
+      <AngleSelector />
+    </div>
+  );
+}
+
+export default App;
